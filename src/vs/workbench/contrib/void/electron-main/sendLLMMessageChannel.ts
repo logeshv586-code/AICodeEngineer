452fceb807a6b1ee448bfddfbf11abb4470c1dab
logesh <logeshv586@gmail.com>
2026-09-01 22:53:33 +0530

    Route API tests through bounded connection checker

--- src/vs/workbench/contrib/void/electron-main/sendLLMMessageChannel.ts
+++ src/vs/workbench/contrib/void/electron-main/sendLLMMessageChannel.ts
@@ -11,7 +11,8 @@ import { Emitter, Event } from '../../../../base/common/event.js';
 import { EventLLMMessageOnTextParams, EventLLMMessageOnErrorParams, EventLLMMessageOnFinalMessageParams, MainSendLLMMessageParams, AbortRef, SendLLMMessageParams, MainLLMMessageAbortParams, ModelListParams, EventModelListOnSuccessParams, EventModelListOnErrorParams, OllamaModelResponse, OpenaiCompatibleModelResponse, MainModelListParams, } from '../common/sendLLMMessageTypes.js';
 import { sendLLMMessage } from './llmMessage/sendLLMMessage.js'
 import { IMetricsService } from '../common/metricsService.js';
-import { sendLLMMessageToProviderImplementation, testLLMConnection } from './llmMessage/sendLLMMessage.impl.js';
+import { sendLLMMessageToProviderImplementation } from './llmMessage/sendLLMMessage.impl.js';
+import { testLLMConnectionFast } from './llmMessage/testLLMConnection.js';
 
 // NODE IMPLEMENTATION - calls actual sendLLMMessage() and returns listeners to it
 
@@ -81,7 +82,7 @@ export class LLMMessageChannel implements IServerChannel {
 				this._callOpenAICompatibleList(params)
 			}
 			else if (command === 'testConnection') {
-				return await testLLMConnection(params)
+				return await testLLMConnectionFast(params)
 			}
 			else {
 				throw new Error(`Void sendLLM: command "${command}" not recognized.`)
